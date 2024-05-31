ba3a28f646fa1e301aac38b2e43317f0d29391df
elijdev <164196443+elijdev@users.noreply.github.com>
2024-05-31 09:41:55 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     var footer = document.getElementById("footer");
-    footer.style.backgroundColor = "rgba(51, 51, 51, 0.5)";
+    footer.style.backgroundColor = "rgba(51, 51, 51, 0.8)";
 });
